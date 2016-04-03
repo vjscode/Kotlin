7e85ef2271ab10a0a887e7667c4abdb0cc89c1c6
Vijay Sailappan <vijay.sailappan@gmail.com>
2016-04-02 19:55:20 -0700

    No more findViewByIds

---
+++ app/src/main/java/vijay/kotlinsamples/MainActivity.kt
@@ -0,0 +1,17 @@
+package vijay.kotlinsamples
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //No more findViewByIds!!
+        txtTitle.setText(R.string.title)
+        txtContent.setText(R.string.content)
+    }
+}
